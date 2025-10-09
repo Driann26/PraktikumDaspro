32fb3a754220a26637d999e212b5b759295a63b4
Driann26 <adriang2610@gmail.com>
2025-10-09 15:18:40 +0700

    Trial Modification 2

---
+++ jobsheet5/ifElseCetakKRS02.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ifElseCetakKRS02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("---Print KRS Siakad---");
+        System.out.print("Enter current semester: ");
+        int semester = sc.nextInt();
+        
+        if (semester == 1) {
+            System.out.println("KRS Semester 1 is displayed");
+        } else if (semester == 2) {
+            System.out.println("KRS Semester 2 is displayed");
+        } else if (semester == 3) {
+            System.out.println("KRS Semester 3 is displayed");
+        } else if (semester == 4) {
+            System.out.println("KRS Semester 4 is displayed");
+        } else if (semester == 5) {
+            System.out.println("KRS Semester 5 is displayed");
+        } else if (semester == 6) {
+            System.out.println("KRS Semester 6 is displayed");
+        } else {
+            System.out.println("Invalid semester entered.");
+        }
+        sc.close();
+    }
+}
