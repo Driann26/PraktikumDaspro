285adce739a4ae89b729f6e3fba11b5415362a90
Driann26 <adriang2610@gmail.com>
2025-10-09 14:22:15 +0700

    Test Modification 1

--- jobsheet5/ifCetakKRS02.java
+++ jobsheet5/ifCetakKRS02.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class ifCetakKRS02 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc  = new Scanner(System.in);
 
-        System.out.println("---Print KRS SIAKAD---");
-        System.out.println("Have the tuition fees been pain in full? (true/false): ");
+        System.out.println("---Print KRS Siakad---");
+        System.out.print("Have the tuition fee been paid? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
         if (uktLunas) {
-            System.out.println("UKT payment has been verified");
+            System.out.println("UKT payment has been verified.");
             System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
+        } else {
+            System.out.println("Registration rejected. Please pay UKT first.");
         }
+
         sc.close();
     }
 }
